48fae70f948c28e623668af3c07345858e2af329
karl-no <karl.publicidade@gmail.com>
2023-04-25 19:02:56 -0300

    test: encrypt_message function tested

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,10 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("xablau", 3) == "bax_ual"
+    assert encrypt_message("xablau", 9) == "ualbax"
+
+    with pytest.raises(TypeError):
+        encrypt_message(3, "xablau")
