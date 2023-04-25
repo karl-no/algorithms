80ff73496ab3f2cd18c57a2c278fe25986cc46f4
karl-no <karl.publicidade@gmail.com>
2023-04-25 17:54:24 -0300

    feat: study_schedule function gets the number of students studying in a certain schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,4 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-
-    raise NotImplementedError
+    number_of_students = 0
+    try:
+        for permanence in permanence_period:
+            if permanence[0] <= target_time <= permanence[1]:
+                number_of_students += 1
+        return number_of_students
+    except TypeError:
+        ...
