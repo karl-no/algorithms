fd7e4f67fa7cb83d373bb317ffc49cf3d9775bed
karl-no <karl.publicidade@gmail.com>
2023-04-26 19:40:44 -0300

    feat: find_duplicate function ready

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,11 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    nums.sort()
+    for n in nums:
+        if type(n) != int or n < 0 or len(nums) < 2:
+            return False
+
+    for n in range(len(nums) - 1):
+        if nums[n] == nums[n + 1]:
+            return nums[n]
+
+    return False
